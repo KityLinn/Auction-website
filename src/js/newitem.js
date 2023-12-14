6fdf9cd16033d0592000bf74677d9780b6a7db97
KityLinn <87649871+KityLinn@users.noreply.github.com>
2023-12-14 02:27:45 +0100

    new item script initial

--- src/js/newitem.js
+++ src/js/newitem.js
@@ -1,1 +1,35 @@
 import { auctionUrls } from "./module.mjs";
+
+const itemUrl = document.querySelector("#item-url");
+const itemTitle = document.querySelector("#item-title")
+const itemDesc = document.querySelector("#item-desc")
+const itemTags = document.querySelector("#item-tags")
+const itemExpire = document.querySelector("#item-expire")
+const postItem = document.querySelector("#post-item")
+
+postItem.addEventListener("click", (e) => {
+    e.preventDefault()
+    const item = {
+        title: itemTitle.value,
+        description: itemDesc.value,
+        tags: itemTags.value,
+        media: itemUrl.value,
+        endsAt: itemExpire.value,
+    };
+    creaListing(auctionUrls.createEntry, item)
+});
+
+
+const creaListing = async (itemUrl, itemData) => {
+    const token = localStorage.getItem("token");
+    const res = await fetch (itemUrl, {
+        method: "post",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify(itemData),
+    });
+    const data = await res.json(); 
+    console.log(data)
+}
